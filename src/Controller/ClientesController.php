efa214d2e8f6251ba64f3b610b924e7ffa15e77a
inforob <inforob@ono.com>
2024-01-04 20:17:18 +0100

    Rutas y controladores

---
+++ src/Controller/ClientesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/personal', name: 'personal')]
+class ClientesController extends AbstractController
+{
+    #[Route('/clientes/{clientId}', name: '_clientes', requirements: ['clientId' => '\d+'], methods: ["GET"])]
+    public function clientes(string $clientId): Response
+    {
+        return $this->render("clientes/index.html.twig",[
+            'clientId' => $clientId,
+        ]);
+    }
+
+    #[Route('/empleados/{empleadosId}', name: '_empleados', requirements: ['empleadosId' => '\d+'], methods: ["GET"])]
+    public function empleados(string $empleadosId): Response
+    {
+        return $this->render("clientes/index.html.twig",[
+            'empleadosId' => $empleadosId
+        ]);
+    }
+}
