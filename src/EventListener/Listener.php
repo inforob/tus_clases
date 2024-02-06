737b2ee25eeca37a109926d34e34ab22a4cbae2e
inforob <inforob@ono.com>
2024-02-06 19:48:26 +0000

    Traducciones segunda parte

---
+++ src/EventListener/Listener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+final class Listener
+{
+    #[AsEventListener(event: KernelEvents::REQUEST,priority: 5000)]
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+
+        $lang = $request->query->get('lang');
+
+        $request->setLocale($lang);
+
+    }
+}
